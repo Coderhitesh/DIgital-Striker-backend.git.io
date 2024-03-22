441459fe63b169aaaa5f67def04e13b2bcdb421c
Hitesh Yadav <hiteshy468@gmail.com>
2024-03-22 17:33:23 +0530

    first commit

---
+++ modals/product.modal.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+
+const productSchema = new mongoose.Schema({
+    name : {
+        type : String,
+        required : true
+    },
+    des : {
+        type : String,
+        required : true
+    },
+    img : {
+        type : String,
+        required : true
+    }
+
+})
+
+const product = mongoose.model('product',productSchema)
+
+module.exports = product;
---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const dotenv = require('dotenv')
+const PORT = process.env.PORT || 3001;
+const connectDb = require('./config/db')
+dotenv.config()
+const productRoutes = require('./routers/route')
+
+app.use(express.json());
+app.use(express.urlencoded({extends:true}))
+
+
+app.use('/api', productRoutes);
+
+app.listen(PORT,()=>{
+    console.log('server run on' , PORT)
+})
+
+connectDb()
