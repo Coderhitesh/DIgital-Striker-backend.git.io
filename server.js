7c4a47a3eea1529bdb0412755f50541d114138a8
Hitesh Yadav <hiteshy468@gmail.com>
2024-03-23 11:44:48 +0530

    add cors

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const PORT = process.env.PORT || 3001;
 const connectDb = require('./config/db')
 dotenv.config()
 const productRoutes = require('./routers/route')
+const cors = require('cors')
+app.use(cors())
 
 app.use(express.json());
 app.use(express.urlencoded({extends:true}))
